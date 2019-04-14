966af22b1b8678f5d1baf6e1ff8d6eff30a58337
Shaun Osborne <shaun.osborne@iocane.com.au>
2019-04-15 08:55:21 +0930

    update standard credentioals code and cleaned

--- GetZabbixHost.ps1
+++ GetZabbixHost.ps1
@@ -2,24 +2,29 @@ Param (
     [Parameter(Mandatory=$true)][string]$hostid
  )
 
-<# All MaaS scripts can begin with this standard logic which is:
-- do we have credentials in xml file?
-- if so, good
-- if not, get them and put them in a new xml file
+<# All Zabbix scripts begin with this standard authorisation logic which is:
+###############################################################
+1) do we have credentials in xml file?
+2) if so, good, check it, load it
+3) if not, get them via dialog and put them in the zabbixredentials.xml
 #>
-if(! (Test-Path "MaaScredentials.xml" -PathType Leaf))
+<# do we have a credentials file? #>
+if(! (Test-Path "zabbizcredentials.xml" -PathType Leaf))
   {
     $creds = Get-Credential $null
-    $creds | Export-CliXML -Path maascredentials.xml
+    $creds | Export-CliXML -Path zabbizcredentials.xml
   }
 
 
 <# Get Credentials and do some basic tests on their validity #>
 $loadedcreds = Import-CliXML "maascredentials.xml"
 if(-not $loadedcreds.Username -or -not $loadedcreds.Username){
-    Write-Output "Credentials file MaasCredentials.xml is invalid - please delete.."
+    Write-Output "Credentials file zabbixredentials.xml is invalid - please delete.."
     exit
 }
+
+<# end of standard authorisation logic
+##############################################################>
 
 <# build login call #>
 $params = '{
@@ -71,14 +76,6 @@ foreach($entry in $entries.result)
 
 
 $entries = $content.result
-#$entries
-
-<#foreach($ent in $entries)
-{
-   $ent
-    Write-Output "______________________________"
-}#>
-#| %{[pscustomobject]$_}
 
 
 [PsObject]$output = @()
@@ -87,14 +84,6 @@ foreach($ent in $entries)
     $output += @{hostid = $ent.hostid;  name = $ent.name; host = $ent.host}
 }
 
-#   $output += @{groupid = $groupid; name = $_.name; hostname = $_host ; hostid = $_.hostid} 
-
-
-# 
-#            Write-Output $key
-#            Write-Output [PsObject]@{dsname = $key; usedpc = $store[$key]['usedpc']; freepc = $store[$key]['freepc'] ; usedGB = $store[$key]['usedGB'] ; freeGB = $store[$key]['freeGB'] ; totalGB = $store[$key]['totalGB'] ; usedTB = $store[$key]['usedTB'] ; freeTB = $store[$key]['freeTB']; totalTB = $store[$key]['totalTB']  }      
-#
-#}
 
 $output  | %{[pscustomobject]$_}
 
