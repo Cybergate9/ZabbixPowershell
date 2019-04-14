65244438b3c2a63f711c29e0090a1ea8ce1ba594
Shaun Osborne <shaun.osborne@iocane.com.au>
2019-04-15 08:53:30 +0930

    credentials standard code updates

--- SetZabbixCredentials.ps1
+++ SetZabbixCredentials.ps1
@@ -1,4 +1,5 @@
-# this just opens a dialog, gathers login details and store the PSCredentials object to xml file in the current directory
+# this just opens a dialog, gathers login details and stores the PSCredentials object to xml file in the current directory
+# all subsequent calls to 'GetZabbixXYZ' scripts will use these stroed credentials
 $creds = Get-Credential $null
-$creds | Export-CliXML -Path maascredentials.xml
+$creds | Export-CliXML -Path zabbixcredentials.xml
 	
